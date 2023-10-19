7d24046c9ff5f23d30e1aa37d92de88075ea10bb
Arun <arun1989@gmail.com>
2023-10-19 23:04:26 +0530

    Initial Commit

---
+++ LockSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LockSwiftUI
+//
+//  Created by Rahul-dev on 19/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LockSwiftUI/LockSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockSwiftUIApp.swift
+//  LockSwiftUI
+//
+//  Created by Rahul-dev on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LockSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
